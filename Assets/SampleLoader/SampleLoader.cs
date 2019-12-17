41ae1a567573cade294af10e2f41116f70901627
Thomas ICHÉ <peeweek@gmail.com>
2019-12-17 17:58:30 +0100

    New Menu

--- Assets/SampleLoader/SampleLoader.cs
+++ Assets/SampleLoader/SampleLoader.cs
@@ -10,26 +10,49 @@ public class SampleLoader : MonoBehaviour
     public Image FullScreenBlack;
     public Text SceneNameText;
     public string ChangeButtonName = "Submit";
-    public float SceneChangeDuration = 60.0f;
 
+    [Header("Scene Loading")]
     [Min(0.002f)]
     public float fadeDuration = 2.0f;
 
-    [Header("Debug")]
+    [Header("Scene Loading")]
+    public Toggle DemoModeToggle;
+    public float DemoModeSceneChangeDuration = 20.0f;
+    public GameObject DemoModeProgressBG;
+    public RectTransform DemoModeProgressBar;
+
+    [Header("Options Menu")]
+    public KeyCode MenuToggleKey = KeyCode.Escape;
+    public RectTransform OptionsMenuTransform;
+    public Button OpenMenuButton;
+    public Button CloseMenuButton;
+
+    [Header("Load Scene Window")]
+    public GameObject LoadSceneWindowRoot;
+
+    [Header("FPS Counter")]
     public GameObject DebugRoot;
     public Text FramerateText;
+    public Toggle FPSCounterToggle;
 
-    int next = 0;
-    bool fading;
-    bool loading;
-    float fadeTTL;
-    // Start is called before the first frame update
+
+    int currentScene = 0;
+    bool m_Fading;
+    bool m_Loading;
+    float m_FadeTTL;
+
     void Start()
     {
         if (SceneManager.sceneCountInBuildSettings > 1)
             StartCoroutine(LoadScene(0));
+
+        SetFPSVisible(false);
+        SetMenuVisible(false);
+        SetLoadSceneWindowVisible(false);
+        SetDemoMode(true);
     }
 
+    #region FPS COUNTER
     void UpdateDebug()
     {
         if (FramerateText == null)
@@ -62,35 +85,160 @@ public class SampleLoader : MonoBehaviour
             return Time.unscaledDeltaTime;
     }
 
+    bool m_FPSVisible = false;
+
+    public void SetFPSVisible(bool visible)
+    {
+        DebugRoot.SetActive(visible);
+        m_FPSVisible = visible;
+        FPSCounterToggle.isOn = visible;
+    }
+
+    public void MenuToggleFPSCounter()
+    {
+        SetFPSVisible(FPSCounterToggle.isOn);
+    }
+
+    #endregion
+
+    #region OPTIONS MENU
+
+    bool m_MenuVisible = false;
+
+    public void SetMenuVisible(bool visible)
+    {
+        if(visible)
+        {
+            if(m_LoadSceneWindowVisible)
+                SetLoadSceneWindowVisible(false);
+
+            OpenMenuButton.gameObject.SetActive(false);
+            OptionsMenuTransform.anchoredPosition = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            OpenMenuButton.gameObject.SetActive(true);
+            OptionsMenuTransform.anchoredPosition = new Vector3(OptionsMenuTransform.sizeDelta.x, 0, 0);
+        }
+
+        m_MenuVisible = visible;
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    #region DEMO MODE
+    bool m_DemoMode = false;
+
+    public void SetDemoMode(bool value)
+    {
+        m_DemoMode = value;
+        DemoModeToggle.isOn = value;
+        DemoModeProgressBG.SetActive(value);
+
+        if (value)
+            m_FadeTTL = DemoModeSceneChangeDuration;
+    }
+
+    public void MenuToggleDemoMode()
+    {
+        SetDemoMode(DemoModeToggle.isOn);
+    }
+
+    public void UpdateDemoMode()
+    {
+        // Pauses the demo mode when in the menu
+        if (m_MenuVisible || m_LoadSceneWindowVisible || m_Loading || m_Fading)
+            return;
+
+        m_FadeTTL -= Time.unscaledDeltaTime;
+
+        DemoModeProgressBar.localScale = new Vector3(1.0f - (m_FadeTTL / DemoModeSceneChangeDuration),1,1);
+
+        if (Input.anyKey || Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f)
+            m_FadeTTL = DemoModeSceneChangeDuration;
+
+        if (!m_Loading && m_FadeTTL < 0.0f)
+            NextScene();
+    }
+
+    #endregion
+
+    #region LOAD SAMPLE WINDOW
+
+    bool m_LoadSceneWindowVisible = false;
+
+    public void SetLoadSceneWindowVisible(bool value)
+    {
+        if (value)
+            SetMenuVisible(false);
+
+        LoadSceneWindowRoot.SetActive(value);
+        m_LoadSceneWindowVisible = value;
+    }
+
+    public void MenuLoadScene(int index)
+    {
+        SetLoadSceneWindowVisible(false);
+        if(index >= 0)
+            SwitchToScene(index);
+    }
+
+    #endregion
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            Application.Quit();
+        if (Input.GetKeyDown(MenuToggleKey))
+        {
+            SetLoadSceneWindowVisible(false);
+            SetMenuVisible(!m_MenuVisible);
+        }
 
-        if (Input.GetKeyDown(KeyCode.F12) && DebugRoot != null)
-            DebugRoot.SetActive(!DebugRoot.activeSelf);
+        if (Input.GetKeyDown(KeyCode.F12))
+            SetFPSVisible(!m_FPSVisible);
+
+        if (Input.GetKeyDown(KeyCode.F11))
+            SetDemoMode(!m_DemoMode);
 
         UpdateDebug();
 
-        fadeTTL -= Time.unscaledDeltaTime;
+        if(m_DemoMode)
+            UpdateDemoMode();
 
-        if (Input.anyKey || Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f)
-            fadeTTL = SceneChangeDuration;
-
-        if (!loading && (Input.GetButton(ChangeButtonName) || Input.touchCount != 0 || (fadeTTL < 0.0f)))
+        if (!m_Loading && (Input.GetButton(ChangeButtonName) || Input.touchCount != 0))
         {
-            int current = next;
-            next = (next + 1) % (SceneManager.sceneCountInBuildSettings - 1);
-
-            if(next != current)
-                StartCoroutine(LoadScene(current, next));
+            NextScene();
         }
+    }
+
+    #region SCENE LOADING
+
+    public void NextScene()
+    {
+        int previous = currentScene;
+        currentScene = (currentScene + 1) % (SceneManager.sceneCountInBuildSettings - 1);
+
+        if (currentScene != previous)
+            StartCoroutine(LoadScene(previous, currentScene));
+    }
+
+    void SwitchToScene(int index)
+    {
+        int previous = currentScene;
+
+        if(index != previous)
+            StartCoroutine(LoadScene(previous, index));
+
+        currentScene = index;
     }
 
     IEnumerator LoadScene(params int[] scene)
     {
-        loading = true;
+        m_Loading = true;
 
         int sceneToUnload = -1, sceneToLoad;
 
@@ -107,7 +255,7 @@ public class SampleLoader : MonoBehaviour
         if(scene.Length == 2) // Need to unload
         {
             StartCoroutine(FadeInCoroutine());
-            while (fading)
+            while (m_Fading)
                 yield return new WaitForEndOfFrame();
 
             AsyncOperation unload = SceneManager.UnloadSceneAsync(sceneToUnload+1);
@@ -125,17 +273,17 @@ public class SampleLoader : MonoBehaviour
         SceneNameText.text = SceneManager.GetSceneAt(1).name;
 
         StartCoroutine(FadeOutCoroutine());
-        while (fading)
+        while (m_Fading)
             yield return new WaitForEndOfFrame();
 
-        loading = false;
-        fadeTTL = SceneChangeDuration;
+        m_Loading = false;
+        m_FadeTTL = DemoModeSceneChangeDuration;
         yield return null;
     }
 
     IEnumerator FadeInCoroutine()
     {
-        fading = true;
+        m_Fading = true;
 
         float alpha = 0.0f;
         while(alpha <= 1.0f)
@@ -147,13 +295,13 @@ public class SampleLoader : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
-        fading = false;
+        m_Fading = false;
         yield return null;
     }
 
     IEnumerator FadeOutCoroutine()
     {
-        fading = true;
+        m_Fading = true;
         float alpha = 1.0f;
         while (alpha >= 0.0f)
         {
@@ -164,8 +312,10 @@ public class SampleLoader : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
-        fading = false;
+        m_Fading = false;
         yield return null;
     }
+
+    #endregion
 
 }
